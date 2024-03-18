8c1f9e76777ad59051ff929866bbb65751cb975b
Diego Alejandro Poveda <alejopov@gmail.com>
2024-03-18 16:26:49 -0500

    add i18n

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
 import './global.css';
 
 import 'react-native-gesture-handler';
+import 'intl-pluralrules';
 
-import RootStack from './src/navigation';
+import LanguageDetector from '@os-team/i18next-react-native-language-detector';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from '~/locales/en/translation.json';
+import translationES from '~/locales/es/translation.json';
+import RootStack from '~/navigation';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  es: {
+    translation: translationES,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    resources,
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default function App() {
   return <RootStack />;
--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,75 +1,63 @@
+// import { Image } from 'expo-image';
 import { useState } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { View, Text, TextInput, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { t } = useTranslation();
 
-  /* return (
-    <View className="flex-1 px-4 pt-6 pb-4 items-center h-full">
-      <View className="flex-1 flex-col justify-center items-center gap-4">
-        <Text className="text-4xl font-bold">Iniciar sesión con tu usuario y contraseña</Text>
-        <TextInput
-          className="bg-indigo-50 w-56 border rounded-md px-3 py-1"
-          placeholder="ejemplo@dominio.com"
-          onChangeText={setEmail}
-          value={email}
-        />
-        <TextInput
-          className="bg-indigo-50 w-56 border rounded-md px-3 py-1"
-          secureTextEntry
-          placeholder="Tu contraseña"
-          value={password}
-          onChangeText={setPassword}
-        />
-        <Button
-          title="Iniciar sesión"
-          onPress={() => {
-            console.log({ email, password });
-          }}
-        />
-      </View>
-    </View>
-  ); */
+  const toggleLoading = () => setLoading(!loading);
 
   return (
     <View className="bg-gray-50 dark:bg-gray-900 h-full">
-      <View className="flex-1 flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <View className="flex-1 flex-col items-center justify-center px-6 py-56 mx-auto">
+        <View className="flex-1 flex-col items-center justify-center">
+          <Image source={require('../../assets/LogoAppTransparent.png')} className="w-20 h-20" />
+          <Text>WestCarPool</Text>
+        </View>
         <View className="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <View className="p-6 space-y-4 md:space-y-6 sm:p-8 gap-4">
             <Text className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
-              Sign in to your account
+              {t('signYourAcccount')}
             </Text>
             <View className="space-y-4 md:space-y-6">
               <View className="mb-3">
                 <Text className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Your email
+                  {t('yourEmail')}
                 </Text>
                 <TextInput
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#007d97] focus:border-[#007d97] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
                 />
               </View>
               <View className="mb-3">
                 <Text className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Password
+                  {t('yourPassword')}
                 </Text>
                 <TextInput
                   secureTextEntry
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#007d97] focus:border-[#007d97] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={password}
+                  onChangeText={setPassword}
                 />
               </View>
-              <TouchableOpacity className="w-full bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
-                <Text className="text-white text-center">Sign in</Text>
+              <TouchableOpacity
+                disabled={loading}
+                className="w-full bg-[#007d97] hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#007d97] dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+                onPress={toggleLoading}>
+                {loading ? (
+                  <ActivityIndicator />
+                ) : (
+                  <Text className="text-white text-center">{t('logIn')}</Text>
+                )}
               </TouchableOpacity>
-              {/* <Text className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don't have an account yet?{' '}
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
-                  Sign up
-                </a>
-              </Text> */}
             </View>
           </View>
         </View>
